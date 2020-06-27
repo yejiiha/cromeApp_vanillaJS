0e8944c83c7f4ca7841c517b64221a17cff44d2f
yezii <1126yezy@gmail.com>
2020-06-27 23:06:31 +0900

    Add Background Images

---
+++ bg.js
@@ -0,0 +1,29 @@
+const body = document.querySelector("body");
+
+const IMG_NUM = 6;
+
+function handleImgLoad() {
+    console.log("finished loading");
+}
+
+function createImage(imgNumber) {
+    const image = new Image();
+
+    image.src = `images/${imgNumber + 1}.jpg`;
+    image.classList.add("bgImage");
+
+    body.prepend(image);
+}
+
+function genRandom() {
+    const number = Math.floor(Math.random() * IMG_NUM);
+    return number;
+}
+
+
+function init() {
+    const randomNum = genRandom();
+    createImage(randomNum);
+}
+
+init();
